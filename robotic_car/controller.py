02500613cea33dbf73d0e9db16a944bd805c9481
Ted <ted.jh@hotmail.com>
2026-05-17 20:20:13 +0100

    feat: key stroke registering added

--- robotic_car/controller.py
+++ robotic_car/controller.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
+import tty
+import termios
 
 import rclpy
 from rclpy.executors import ExternalShutdownException
@@ -18,32 +21,53 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
+def get_key(settings):
+    tty.setraw(sys.stdin.fileno())
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
 class Controller(Node):
 
     def __init__(self):
         super().__init__('controller')
         self.publisher_ = self.create_publisher(String, 'velocity', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.get_logger().info('Teleop node started, use keys to publish, Ctrl+C to quit')
+        #timer_period = 0.5  # seconds
+        #self.timer = self.create_timer(timer_period, self.timer_callback)
+        #self.i = 0
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    #def timer_callback(self):
+    #    msg = String()
+    #    msg.data = 'Hello World: %d' % self.i
+    #    self.publisher_.publish(msg)
+    #    self.get_logger().info('Publishing: "%s"' % msg.data)
+    #    self.i += 1
+    def run(self):
+        settings = termios.tcgetattr(sys.stdin)
+        try:
+            while rclpy.ok():
+                key = get_key(settings)
+
+                if key == '\x03':  # Ctrl+C
+                    break
+
+                msg = String()
+                msg.data = key
+                self.publisher_.publish(msg)
+                self.get_logger().info(f'Published key: {repr(key)}')
+        finally:
+            # Restore terminal settings on exit
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 
 def main(args=None):
-    try:
-        with rclpy.init(args=args):
-            controller = Controller()
-
-            rclpy.spin(controller)
-    except (KeyboardInterrupt, ExternalShutdownException):
-        pass
+    with rclpy.init(args=args):
+        controller = Controller()
+        try:
+            controller.run()
+        finally:
+            controller.destroy_node()
 
 
 if __name__ == '__main__':
